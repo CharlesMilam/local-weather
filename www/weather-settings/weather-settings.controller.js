cd1c68d6383ccefef821270c78a02b1633606833
Charles Milam <charles.milam@gmail.com>
2015-09-12 23:26:12 -0500

    Modify weather settings controller to make use of WeatherConditions service. Pulls data appropriately, but does not update the current weather tab.

--- www/weather-settings/weather-settings.controller.js
+++ www/weather-settings/weather-settings.controller.js
@@ -1,5 +1,5 @@
 angular.module("LocalWeather.weather-settings")
-.controller('SettingsCtrl', function($scope, ChangeUnits) {
+.controller('SettingsCtrl', function($scope, ChangeUnits, WeatherConditions) {
   $scope.settings = {
     enableMetric: ChangeUnits.getIsMetric()
   };
@@ -7,6 +7,26 @@ angular.module("LocalWeather.weather-settings")
     ChangeUnits.setIsMetric($scope.settings.enableMetric);
     $scope.metricNotificationChange();
     // $scope.getConditions;
+    WeatherConditions()
+    .then(function success(resp) {
+      console.log("data", resp.data);
+      var data = resp.data;
+      $scope.conditions = {
+        city: data.name,
+        temp: data.main.temp,
+        humidity: data.main.humidity,
+        sky: data.weather[0].description,
+        icon: data.weather[0].id,
+        wind: {
+          speed: data.wind.speed,
+          dir: data.wind.deg
+        },
+        currdate: new Date(data.dt)
+      };
+    }),
+    function error(data) {
+      return console.log("ERROR", data);
+    }
   };
 
   $scope.metricNotificationChange = function() {
